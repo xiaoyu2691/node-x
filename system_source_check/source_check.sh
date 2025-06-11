5fe3d7f3fd8840c3e248275e3d8384a28fcb87ff
xiaoyu2691 <xiaoyu2691@gmail.com>
2025-06-11 11:57:47 +0800

    Update source_check.sh

--- system_source_check/source_check.sh
+++ system_source_check/source_check.sh
@@ -66,32 +66,34 @@ fi
 echo -e "\n\e[36m===内存使用情况===\e[0m"
 memory_info=$(free -h | awk 'NR==2 {print "已使用: "$3", 空闲: "$7}')
 echo -e "\e[37m$memory_info\e[0m"
-free_ram=$(free | awk 'NR==2 {print $7}') # 获取空闲内存大小
+free_ram=$(free | awk 'NR==2 {print $7}') # 获取空闲内存大小（单位为KiB）
+free_ram_gb=$(echo "scale=2; $free_ram / 1024 / 1024" | bc) # 转换为GB
 # 检查空闲内存是否小于阈值
-if [ "$free_ram" -lt "$ram_threshold" ]; then
-    if [ "$free_ram" -eq 0 ]; then
-        echo -e "\e[31m警告：内存资源已经占用完全，没有空闲资源，目前资源为 $ram_threshold\e[0m"
-        exit 1
+if [ "$(echo "$free_ram_gb < $ram_threshold" | bc)" -eq 1 ]; then
+    if [ "$(echo "$free_ram_gb == 0" | bc)" -eq 1 ]; then
+        echo -e "\e[31m警告：内存资源已经占用完全，没有空闲资源，目前资源为 $ram_threshold GB\e[0m"
     else
-        echo -e "\e[31m警告：空闲RAM不足,节点可能安装失败，请注意系统资源分配！\e[0m"
+        echo -e "\e[31m警告：空闲RAM不足\e[0m"
     fi
-else
-    echo "当前空闲内存:$free_ram"
+    exit 1
 fi
 
+# 显示磁盘使用情况
 # 显示磁盘使用情况
 echo -e "\n\e[36m===磁盘使用情况===\e[0m"
 disk_info=$(df -h / | awk 'NR==2 {print "已使用: "$3", 空闲: "$4}')
 echo -e "\e[37m$disk_info\e[0m"
 free_rom=$(df / | awk 'NR==2 {print $4}' | sed 's/G//') # 获取空闲磁盘大小（单位为G）
+free_rom_kb=$(df / | awk 'NR==2 {print $4}') # 获取空闲磁盘大小（单位为KiB）
+free_rom_gb=$(echo "scale=2; $free_rom_kb / 1024 / 1024" | bc) # 转换为GB
 
-if [ "$free_rom" -lt "$rom_threshold" ]; then
-    if [ "$free_rom" -eq 0 ]; then
+# 检查空余磁盘是否小于阈值
+if [ "$(echo "$free_rom_gb < $rom_threshold" | bc)" -eq 1 ]; then
+    if [ "$(echo "$free_rom_gb == 0" | bc)" -eq 1 ]; then
         echo -e "\e[31m警告：磁盘空间已经占用完全，没有空闲资源，目前资源为 $rom_threshold G\e[0m"
     else
         echo -e "\e[31m警告：服务器磁盘空间不足，部署该节点最低磁盘为 $rom_threshold G\e[0m"
     fi
     exit 1
-else
-    echo "当前空闲磁盘大小:$free_rom"
 fi
+
